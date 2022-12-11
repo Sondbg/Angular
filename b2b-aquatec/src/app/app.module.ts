18ab6fa2234fe20ec55b599c78188bb8936eb535
sondbg <sondbg@abv.bg>
2022-12-12 00:08:55 +0200

    Header, Login components

--- b2b-aquatec/src/app/app.module.ts
+++ b2b-aquatec/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { CoreModule } from './core/core.module'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
